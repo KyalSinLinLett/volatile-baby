9c2b153d4a34661a7442c58f5ebacae25ca648ca
KyalSinLinLett <alexsin0012@gmail.com>
2021-08-09 16:07:43 +0630

    change url

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :realtime_chat, RealtimeChatWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "still-taiga-11855.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "volatile-baby.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
